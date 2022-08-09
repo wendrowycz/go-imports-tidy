ce81a42d2f2a23beddd5d7186b38f60d1b83d1be
Bartosz Grzesiak <bartosz.grzesiak@gmail.com>
2022-08-09 09:38:34 +0200

    Add support for build 2022.2

--- src/main/java/icons/PluginIcons.java
+++ src/main/java/icons/PluginIcons.java
@@ -5,5 +5,5 @@ import com.intellij.openapi.util.IconLoader;
 import javax.swing.*;
 
 public interface PluginIcons {
-    Icon PluginIcon = IconLoader.getIcon("/icons/pluginIcon.svg", PluginIcons.class);
+    Icon PluginIcon = IconLoader.getIcon("/icons/gofer.svg", PluginIcons.class);
 }
